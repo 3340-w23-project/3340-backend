d89bb97c1c5f16c89463a91e30669d87803b355f
Jeremie Bornais <jeremiejbornais@gmail.com>
2023-03-06 22:03:31 +0000

    integrare simple JWT stuff from article

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,5 +1,7 @@
 import json
+from datetime import timedelta
 from flask import Flask
+from flask_jwt_extended import JWTManager
 
 # getting config details
 with open('config.json') as f:
@@ -8,5 +10,10 @@ with open('config.json') as f:
 # initializing Flask
 app = Flask(__name__)
 app.config['SECRET_KEY'] = data['secret_key']
+
+# jwt config
+app.config["JWT_SECRET_KEY"] = data['jwt_secret_key']
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+jwt = JWTManager(app)
 
 from backend import routes
