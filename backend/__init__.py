bfc4288adaaf9580d96c7d54b75d5a01b3d0d64d
Jeremie Bornais <jeremiejbornais@gmail.com>
2023-03-07 05:38:16 +0000

    added basic database stuff

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,6 +2,7 @@ import json
 from datetime import timedelta
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
 
 # getting config details
 with open('config.json') as f:
@@ -15,5 +16,13 @@ app.config['SECRET_KEY'] = data['secret_key']
 app.config["JWT_SECRET_KEY"] = data['jwt_secret_key']
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
 jwt = JWTManager(app)
+
+# create the extension
+db = SQLAlchemy()
+
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = data['database_uri']
+# initialize the app with the extension
+db.init_app(app)
 
 from backend import routes
--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 import json
 import sys
 
-from backend import app
+from backend import app, db
 
 # getting config details
 with open('config.json') as f:
@@ -11,11 +11,29 @@ with open('config.json') as f:
 
 # running site
 if __name__=='__main__':
-    # run this command with any additional arg to run in production
-    if len(sys.argv) > 1:
-        print('<< PROD >>')
-        os.system(f"gunicorn -b '0.0.0.0:{data['port']}' backend:app")
-    # or just run without an additional arg to run in debug
-    else:
+    # without an additional arg to run in debug
+    if len(sys.argv) < 2:
         print('<< DEBUG >>')
         app.run(debug=True)
+
+    elif len(sys.argv) > 2:
+        print("Too many arguments, exiting")
+        exit(1)
+    else:
+        # run this command with the flag "db-setup" to set up the database
+        if sys.argv[1] == 'db-setup':
+            print("setting up db ...")
+            print("importing models ...")
+            from backend import models
+            print("creating tables...")
+            with app.app_context():
+                db.create_all()
+            print("done!")
+            exit(1)
+        elif sys.argv[1] == 'prod':
+            # run this command with the "prod" flag to run in prod
+            print('<< PROD >>')
+            os.system(f"gunicorn -b '0.0.0.0:{data['port']}' backend:app")
+        else:
+            print(f"unknown option '{sys.argv[1]}', exiting")
+            exit(1)
