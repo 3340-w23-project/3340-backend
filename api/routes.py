cc0c28b8f0a179ae27a6f64fd076c1c229f3d218
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2023-04-20 22:18:54 -0400

    remove: Refresh Expiring Tokens

--- api/routes.py
+++ api/routes.py
@@ -101,27 +101,25 @@ def logout():
     return response
 
 
-@app.after_request
-def refresh_expiring_jwts(response):
-    try:
-        exp_timestamp = get_jwt()["exp"]
-        now = datetime.now(timezone.utc)
-        target_timestamp = datetime.timestamp(now + timedelta(hours=6))
-        if target_timestamp > exp_timestamp:
-            access_token = create_access_token(identity=get_jwt_identity())
-            data = response.get_json()
-            if type(data) is dict:
-                data["access_token"] = access_token
-                response.data = json.dumps(data)
-        return response
-    except (RuntimeError, KeyError):
-        # Case where there is not a valid JWT. Just return the original respone
-        return response
+# @app.after_request
+# def refresh_expiring_jwts(response):
+#     try:
+#         exp_timestamp = get_jwt()["exp"]
+#         now = datetime.now(timezone.utc)
+#         target_timestamp = datetime.timestamp(now + timedelta(hours=6))
+#         if target_timestamp > exp_timestamp:
+#             access_token = create_access_token(identity=get_jwt_identity())
+#             data = response.get_json()
+#             if type(data) is dict:
+#                 data["access_token"] = access_token
+#                 response.data = json.dumps(data)
+#         return response
+#     except (RuntimeError, KeyError):
+#         # Case where there is not a valid JWT. Just return the original respone
+#         return response
 
 # this is just a dummy endpoint to check if your JWT auth is working
 # it will return back the username associated with your JWT token
-
-
 @app.route('/identity')
 @jwt_required()
 def my_profile():
